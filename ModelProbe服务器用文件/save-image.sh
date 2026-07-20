484c2a8a7d841d85833778e9aa79b21f78eaff69
constanji <875995534@qq.com>
2026-07-20 15:57:26 +0800

    Untrack ModelProbe server files

--- ModelProbe服务器用文件/save-image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-cd "$(dirname "$0")"
-TAG="${TAG:-modelprobe:result}"
-OUT="${OUTPUT:-modelprobe-server-amd64.tar}"
-docker save -o "$OUT" "$TAG"
-ls -lh "$OUT"
